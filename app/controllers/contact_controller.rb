ee4ff66ccb872fe0908ee866fb5db034bf0d5fb0
Eduardo <eduardo.rodrigues@rga.com.br>
2013-04-30 11:40:58 -0300

    initial

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+class ContactController < ApplicationController
+ 
+	# 
+  # Tela inicial do site
+  # 
+  # 
+  # def index
+  #   Teste.new({:name=>"teste"}).save
+  #   render json: {:teste=>Teste.find_one.name}
+  # end
+
+
+
+
+
+ 
+end
